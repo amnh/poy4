b7eee56a0ede6223c87b06411a554c6e122e3f28
Andres Varon <avaron@amnh.org>
2007-09-06 16:56:32 -0400

    [svn] Improvements:     Updated the win32 compilation process to produce the installers     automatically.

--- win32_build.sh
+++ win32_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=/cygdrive/c/ocamlmgw/3_10_0/bin:$PATH
+export PATH=/home/andres/minglibs/bin:/cygdrive/c/ocamlmgw/3_10_0/bin:$PATH
 source $HOME/.keychain/${HOSTNAME}-sh
 
 if ! svn update; then
@@ -9,9 +9,16 @@ fi
 
 cp ./config.win32 config
 
+function compile_executable {
+
 cd ./src
 
 cp ./Makefile.win32 Makefile
+
+if ! make clean; then
+    echo "I could not clean up the distribution ..."
+    exit 1
+fi
 
 if ! make depend; then
     echo "I could not make depend ..."
@@ -23,7 +30,26 @@ if ! make poy; then
     exit 1
 fi
 
-if ! scp poy.exe samson:poy_distro/distro_generation_scripts/; then
+cd ../
+}
+
+compile_executable
+
+if ! cp -f ./src/poy.exe /cygdrive/c/poy_distribution/bin/ncurses_poy.exe; then
+    echo "I could not replace the poy executable in the distribution"
+    exit 1
+fi
+
+sed -e "s/ncurses/html/" config.win32 > config
+compile_executable
+if ! cp -f ./src/poy.exe /cygdrive/c/poy_distribution/bin/seq_poy.exe; then
+    echo "I could not replace the executable in the distribution"
+    exit 1
+fi
+rm -f /cygdrive/c/POY_Installer.msi
+./create_installers.bat
+if ! scp /cygdrive/c/POY_Installer.msi samson:poy_distro/distro_generation_scripts/; then
     echo "I could not copy the resulting executable in newlila2!"
     exit 1
 fi
+
