a3045eadccd8da5719b185b4d972e6266c8369e7
vinh <devnull@localhost>
2007-12-26 10:41:21 -0500

    [svn] Improvement:     Parameter "max_3d_len" is added which is the maximum     sequence length to align 3 sequences in order to     reduce the time consuming for iterative pass for chromosomes.

--- src/tags.ml
+++ src/tags.ml
@@ -17,7 +17,7 @@
 (* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301   *)
 (* USA                                                                        *)
 
-let () = SadmanOutput.register "Tags" "$Revision: 2265 $"
+let () = SadmanOutput.register "Tags" "$Revision: 2500 $"
 
 type tag = string
 type value = tag
@@ -141,6 +141,7 @@ module Characters = struct
     let swap_med = "swap_med"
     let approx = "approx"
     let symmetric = "symmetric"
+    let max_3d_len = "max_3d_len"
 end
 
 module Nodes = struct
