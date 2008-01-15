146b93f7a88e55f60abac60fcbbf9c01be67065b
Andres Varon <avaron@amnh.org>
2008-01-15 12:22:53 -0500

    [svn] Improvements:     Fixed the compilation procedures for Windows by removing the unelegant     steps.
        Updated the source version of PDCurses to 3.3 (we where using 2.8). This
        improves performance and overall experience for the end users.

--- win32_build.sh
+++ win32_build.sh
@@ -2,7 +2,7 @@
 # The environment MACHOST holds the URL of the macintosh host running the
 # virtual machine. The only argument contains the desired extra flags for the
 # configuration script (for example long sequence support). 
-export PATH=/home/andres/minglibs/bin:/cygdrive/c/ocamlmgw/3_10_0/bin:$PATH
+export PATH=/home/andres/minglibs/bin:/cygdrive/c/ocamlmgw/3_10_1/bin:$PATH
 source $HOME/.keychain/${HOSTNAME}-sh
 
 if ! svn update; then
@@ -33,14 +33,14 @@ cd ../
 }
 
 # We first compile the regular ncurses interface
-./configure $1 --enable-xslt --with-extras="/home/andres/pdcurs28/*.o /home/andres/libxml2-2.6.30/*.o /home/andres/libxslt-1.1.22/libxslt/*.o" --enable-interface=ncurses 
+./configure $1 --enable-xslt --enable-interface=ncurses CFLAGS="-O3 -L/home/andres/PDCurses-3.3/win32/ -I /home/andres/PDCurses-3.3/"
 if ! cp -f ./src/poy.exe /cygdrive/c/poy_distribution/bin/ncurses_poy.exe; then
     echo "I could not replace the poy executable in the distribution"
     exit 1
 fi
 
 # Now we compile the html interface
-./configure $1 --enable-xslt --enable-interface=html
+./configure $1 --enable-xslt --enable-interface=html CFLAGS="-O3"
 if ! cp -f ./src/poy.exe /cygdrive/c/poy_distribution/bin/seq_poy.exe; then
     echo "I could not replace the executable in the distribution"
     exit 1
