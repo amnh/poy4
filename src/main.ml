fb765d7c922baeb366709c28cfc0f87298d5f4d1
Andres Varon <avaron@amnh.org>
2008-06-10 15:55:50 -0400

    Removed debug=true which was left by mistake.

--- src/main.ml
+++ src/main.ml
@@ -49,7 +49,7 @@ let () =
     Arguments.usage 
 
 (** Catch errors or not;  helpful for debugging *)
-let debug_pass_errors = true
+let debug_pass_errors = false
 
 let () =
     let out = Status.user_message Status.Information in
